56eac096a76d3c4bbd957607e1b181dd11ec983a
hiyoojeong <hiyoojeong@gmail.com>
2022-11-17 02:11:59 +0900

    사장용 메뉴 - 초대코드 보내기 추가

--- lib/bar/Menubar.dart
+++ lib/bar/Menubar.dart
@@ -6,6 +6,8 @@ import 'AddWorkSpace.dart';
 class MenuBar_manager extends StatelessWidget {
   const MenuBar_manager({Key? key}) : super(key: key);
 
+  final int MAINCOLOR = 0xffE94869;
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -105,44 +107,117 @@ class MenuBar_manager extends StatelessWidget {
               ],
             ),
           ),
-          Container(
-            height: 50,
-            margin: EdgeInsets.fromLTRB(30, 6, 30, 6),
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            decoration: BoxDecoration(
-              color: Color(0xffF4F4F4),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('초대코드 보내기'),
-              ],
+          GestureDetector(
+            onTap: () {
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                        content: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                            child: Text(
+                              "초대코드 입력하기",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelStyle: TextStyle(
+                                color: Color(MAINCOLOR),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2, color: Color(MAINCOLOR)),),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  color: Color(MAINCOLOR),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                      /*
+                          이 시점에 사장한테 입사요청
+                          */
+                                    },
+                                    child: Text(
+                                      "입사요청",
+                                      style: TextStyle(color: Colors.white),
+                                    )),
+                              ),
+                              Container(
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  color: Color(MAINCOLOR),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text(
+                                      "닫기",
+                                      style: TextStyle(color: Colors.white),
+                                    )),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ));
+                  });
+            },
+            child: Container(
+              height: 50,
+              margin: EdgeInsets.fromLTRB(30, 6, 30, 6),
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              decoration: BoxDecoration(
+                color: Color(0xffF4F4F4),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('초대코드 보내기'),
+                ],
+              ),
             ),
           ),
           GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const WorkerManager()),
-                );
-              },
-              child: Container(
-                height: 50,
-                margin: EdgeInsets.fromLTRB(30, 6, 30, 6),
-                padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                decoration: BoxDecoration(
-                  color: Color(0xffF4F4F4),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('로그아웃', style: TextStyle(color: Colors.red)),
-                  ],
-                ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const WorkerManager()),
+              );
+            },
+            child: Container(
+              height: 50,
+              margin: EdgeInsets.fromLTRB(30, 6, 30, 6),
+              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              decoration: BoxDecoration(
+                color: Color(0xffF4F4F4),
+                borderRadius: BorderRadius.circular(10),
               ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('로그아웃', style: TextStyle(color: Colors.red)),
+                ],
+              ),
+            ),
           )
         ],
       ),
@@ -246,28 +321,27 @@ class MenuBar_worker extends StatelessWidget {
             ),
           ),
           GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const WorkerManager()),
-                );
-              },
-              child: Container(
-                height: 50,
-                margin: EdgeInsets.fromLTRB(30, 6, 30, 6),
-                padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                decoration: BoxDecoration(
-                  color: Color(0xffF4F4F4),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('로그아웃', style: TextStyle(color: Colors.red)),
-                  ],
-                ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const WorkerManager()),
+              );
+            },
+            child: Container(
+              height: 50,
+              margin: EdgeInsets.fromLTRB(30, 6, 30, 6),
+              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              decoration: BoxDecoration(
+                color: Color(0xffF4F4F4),
+                borderRadius: BorderRadius.circular(10),
               ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('로그아웃', style: TextStyle(color: Colors.red)),
+                ],
+              ),
+            ),
           )
         ],
       ),
