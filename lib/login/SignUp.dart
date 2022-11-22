dabf72e81dcd27d1dd39dc0f7b46b175ed4d502c
moonyeju <moonyeju8510@gmail.com>
2022-11-22 14:48:56 +0900

    회원가입 조건 수정

--- lib/login/SignUp.dart
+++ lib/login/SignUp.dart
@@ -20,10 +20,13 @@ class SignUp extends State<SignUpPage> {
   SignUp(this.person);
 
   final int MAINCOLOR = 0xffE94869;
+  final _formkey = GlobalKey<FormState>();
   String name = '';
   String id = '';
-  String password = '';
+  String password1 = '';
+  String password2 = '';
   String phonenumber = '';
+  int check = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -42,12 +45,19 @@ class SignUp extends State<SignUpPage> {
         body: Container(
           padding: const EdgeInsets.all(40.0),
           child: Form(
+            key: _formkey,
             child: Column(
               children: <Widget>[
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "이름을 입력해주세요";
+                    }
+                    return null;
+                  },
                   onChanged: (text) {
                     setState(() {
-                      name= text;
+                      name = text;
                     });
                   },
                   decoration: InputDecoration(
@@ -61,16 +71,29 @@ class SignUp extends State<SignUpPage> {
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(width: 2, color: Color(MAINCOLOR)),
                     ),
-                    hintText: 'EX) 김철수',
+                    hintText: 'EX) 안수진',
                   ),
                 ),
                 SizedBox(
                   height: 13,
                 ),
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "이메일아이디를 입력해주세요";
+                    }
+
+                    if (!RegExp(
+                            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                        .hasMatch(value)) {
+                      return '잘못된 이메일 형식입니다.';
+                    }
+
+                    return null;
+                  },
                   onChanged: (text) {
                     setState(() {
-                      id= text;
+                      id = text;
                     });
                   },
                   decoration: InputDecoration(
@@ -91,9 +114,18 @@ class SignUp extends State<SignUpPage> {
                   height: 13,
                 ),
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "비밀번호를 입력해주세요";
+                    }
+                    if (value.length < 8) {
+                      return '8자 이상 입력해주세요!';
+                    }
+                    return null;
+                  },
                   onChanged: (text) {
                     setState(() {
-                      password = text;
+                      password1 = text;
                     });
                   },
                   obscureText: true, // 비밀번호를 적을때 안보이도록
@@ -115,6 +147,17 @@ class SignUp extends State<SignUpPage> {
                   height: 13,
                 ),
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "비밀번호를 재입력해주세요";
+                    }
+                    return null;
+                  },
+                  onChanged: (text) {
+                    setState(() {
+                      password2 = text;
+                    });
+                  },
                   obscureText: true, // 비밀번호를 적을때 안보이도록
                   decoration: InputDecoration(
                       icon: Icon(
@@ -141,19 +184,109 @@ class SignUp extends State<SignUpPage> {
                       primary: Color(MAINCOLOR),
                       alignment: Alignment.center,
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      if (password1 == password2) {
+                        check = 1;
+                        showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                content: SingleChildScrollView(
+                                  child: Column(children: [
+                                    Container(
+                                      margin:
+                                      EdgeInsets.fromLTRB(20, 20, 20, 20),
+                                      child: Text(
+                                        "비밀번호가 일치합니다",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 35,
+                                      decoration: BoxDecoration(
+                                        color: Color(MAINCOLOR),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            /*
+                          이 시점에 사장한테 대타요청
+                          */
+                                          },
+                                          child: Text(
+                                            "닫기",
+                                            style:
+                                            TextStyle(color: Colors.white),
+                                          )),
+                                    ),
+                                  ]),
+                                ),
+                              );
+                            });
+                      } else {
+                        showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                content: SingleChildScrollView(
+                                  child: Column(children: [
+                                    Container(
+                                      margin:
+                                          EdgeInsets.fromLTRB(20, 20, 20, 20),
+                                      child: Text(
+                                        "비밀번호가 일치하지 않습니다",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 35,
+                                      decoration: BoxDecoration(
+                                        color: Color(MAINCOLOR),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            /*
+                          이 시점에 사장한테 대타요청
+                          */
+                                          },
+                                          child: Text(
+                                            "닫기",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          )),
+                                    ),
+                                  ]),
+                                ),
+                              );
+                            });
+                      }
+                    },
                     child: Text('확인'),
                   ),
                 ),
                 TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "전화번호를 입력해주세요";
+                    }
+                    if (!RegExp('[0-9]').hasMatch(value) || !(value.length==11)) {
+                      return '잘못된 전화번호 형식입니다.';
+                    }
+                    return null;
+                  },
                   onChanged: (text) {
                     setState(() {
-                      phonenumber= text;
+                      phonenumber = text;
                     });
                   },
-                  inputFormatters: [
-                    FilteringTextInputFormatter(RegExp('[0-9]'), allow: false),
-                  ],
+                  // inputFormatters: [
+                  //   FilteringTextInputFormatter(RegExp('[0-9]'), allow: false),
+                  // ],
                   decoration: InputDecoration(
                     icon: Icon(
                       Icons.call,
@@ -182,10 +315,53 @@ class SignUp extends State<SignUpPage> {
                         alignment: Alignment.center,
                         textStyle: const TextStyle(fontSize: 15)),
                     onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => CalendarPage_worker()));
+                      if ((_formkey.currentState!.validate())) {
+                        if(check==1){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CalendarPage_worker()));
+                      }else{
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  content: SingleChildScrollView(
+                                    child: Column(children: [
+                                      Container(
+                                        margin:
+                                        EdgeInsets.fromLTRB(20, 20, 20, 20),
+                                        child: Text(
+                                          "비밀번호 확인버튼을 누르세요",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ),
+                                      Container(
+                                        height: 35,
+                                        decoration: BoxDecoration(
+                                          color: Color(MAINCOLOR),
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        child: TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                              /*
+                          이 시점에 사장한테 대타요청
+                          */
+                                            },
+                                            child: Text(
+                                              "닫기",
+                                              style:
+                                              TextStyle(color: Colors.white),
+                                            )),
+                                      ),
+                                    ]),
+                                  ),
+                                );
+                              });
+                        }
+                      }
                     },
                     child: Text('회원가입'),
                   ),
