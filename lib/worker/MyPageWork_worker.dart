d02f7dd9b6a1b9899776c5204a4bd254cb6f2538
hiyoojeong <hiyoojeong@gmail.com>
2022-11-28 00:20:13 +0900

    근무기록 수정 - 민지

--- lib/worker/MyPageWork_worker.dart
+++ lib/worker/MyPageWork_worker.dart
@@ -1,24 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import '../alert/AlertPage_worker.dart';
-import '../bar/Bottombar.dart';
-import '../bar/Menubar.dart';
+//import '../alert/AlertPage_worker.dart';
+//import '../bar/Bottombar.dart';
+//import '../bar/Menubar.dart';
 
 // 알바용 마이페이지 - 근무기록
-class MyPageWork_worker extends StatelessWidget {
+class MyPageWork_worker extends StatefulWidget {
   const MyPageWork_worker({Key? key}) : super(key: key);
 
+  @override
+  State<MyPageWork_worker> createState() => _MyPageWork_worker();
+}
+
+class _MyPageWork_worker extends State<MyPageWork_worker> {
   final int MAINCOLOR = 0xffE94869;
   final int SUBCOLOR = 0xffF4F4F4;
+
+  List<WorkMonthInfo> monthList = [];
+  List<WorkInfo> infoList = [];
+  String token = "", urlsrc = "";
+  int userId = 0, month = 0;
+
+  _fetchMonth() async {
+    // 저장해둔 token 가져오기
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    token = (prefs.getString('token') ?? "null");
+    urlsrc = (prefs.getString('urlsrc') ?? "null");
+    userId = (prefs.getInt('userId') ?? 0);
+    print("token: " + token);
+    print("urlsrc: " + urlsrc);
+    print("userId: " + userId.toString());
+
+    // userId를 바탕으로 2022년 달 요청
+    String url = "http://${urlsrc}/albba/commute/list/${userId}/2022";
+    Map<String, String> headers = {"authorization": "Bearer ${token}"};
+    var response = await http.get(Uri.parse(url), headers: headers);
+    var responseBody = utf8.decode(response.bodyBytes);
+    print(responseBody);
+
+    if (response.statusCode == 200) {
+      // 요청 성공
+      // 전달받은 값 저장
+      setState(() {
+        List<dynamic> body = json.decode(responseBody);
+        monthList =
+            body.map((dynamic item) => WorkMonthInfo.fromJson(item)).toList();
+      });
+    } else {
+      // 요청 실패
+    }
+  }
+
+  _fetchInfo(int selectedMonth) async {
+    // 저장해둔 token 가져오기
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    token = (prefs.getString('token') ?? "null");
+    urlsrc = (prefs.getString('urlsrc') ?? "null");
+    userId = (prefs.getInt('userId') ?? 0);
+    month = (prefs.getInt('month') ?? 0);
+    print("token: " + token);
+    print("urlsrc: " + urlsrc);
+    print("userId: " + userId.toString());
+    print("month: " + month.toString());
+
+    // userId를 바탕으로 2022년 해당 달의 정보 요청
+    String url = "http://${urlsrc}/albba/commute/list/${userId}/2022/${selectedMonth}";
+    Map<String, String> headers = {"authorization": "Bearer ${token}"};
+    var response = await http.get(Uri.parse(url), headers: headers);
+    var responseBody = utf8.decode(response.bodyBytes);
+    print(responseBody);
+
+    if (response.statusCode == 200) {
+      // 요청 성공
+      // 전달받은 값 저장
+      setState(() {
+        List<dynamic> body = json.decode(responseBody);
+        infoList = body.map((dynamic item) => WorkInfo.fromJson(item)).toList();
+      });
+    } else {
+      // 요청 실패
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchMonth();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Scaffold(
-        drawer: MenuBar_worker(),
+      home: Scaffold(
+        //drawer: MenuBar_manager(),
         appBar: AppBar(
           iconTheme: IconThemeData(color: Color(MAINCOLOR)),
-          title: Text('근무기록',
+          title: Text('근무 기록',
               style: TextStyle(
                   color: Color(MAINCOLOR),
                   fontWeight: FontWeight.w700,
@@ -31,123 +114,118 @@ class MyPageWork_worker extends StatelessWidget {
               color: Color(MAINCOLOR),
               iconSize: 30,
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const AlertPage_worker()),
-                );
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //       builder: (context) => const AlertPage_manager()),
+                // );
               },
             )
           ],
         ),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) =>
-              EntryItem(data[index]),
-          itemCount: data.length,
-        ),
-        bottomNavigationBar: BottomBar_worker(),
+        body: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: Row(
+                children: [
+                  Text(
+                    "2022년",
+                    style: TextStyle(
+                      color: Color(MAINCOLOR),
+                      fontSize: 17,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.arrow_drop_down),
+                    onPressed: () {
+                      _fetchMonth();
+                      //_fetchInfo를 해줘야 할까..?
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    for (int i = 0; i < monthList.length; i++)
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: Row(
+                          children: [
+                            Text(monthList[i].month.toString()),
+                            IconButton(
+                              icon: Icon(Icons.arrow_drop_down),
+                              onPressed: () {
+                                setState(() {
+                                  _fetchInfo(monthList[i].month);
+                                  Column(
+                                    children: [
+                                      for (int j = 0; j < infoList.length; j++)
+                                        Text(infoList[j].info),
+                                    ],
+                                  );
+                                });
+                              },
+                            ),
+                            /*Container(
+                              child: Column(
+                                children: [
+                                  for (int j = 0; j < infoList.length; j++)
+                                    Column(
+                                      children: [
+                                        Text(infoList[j].info),
+                                      ],
+                                    ),
+                                ],
+                              ),
+                            ),*/
+                          ],
+                        ),
+                      ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ), // bottomNavigationBar: BottomBar_worker(),
       ),
     );
   }
 }
 
-// One entry in the multilevel list displayed by this app.
-class Entry {
-  Entry(this.title, [this.children = const <Entry>[]]);
+class WorkMonthInfo {
+  // 근무일정 클래스
+  int month = 0; // 달 받아오기
 
-  final String title;
-  final List<Entry> children;
-}
-
-// The entire multilevel list displayed by this app.
-final List<Entry> data = <Entry>[
-  Entry(
-    '2022년',
-    <Entry>[
-      Entry(
-        '3월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],
-      ),
-      Entry('4월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],),
-      Entry('5월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],),
-    ],
-  ),
-  Entry(
-    '2021년',
-    <Entry>[
-      Entry('1월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],),
-      Entry('2월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],),
-    ],
-  ),
-  Entry(
-    '2020년',
-    <Entry>[
-      Entry('10월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],),
-      Entry('11월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],),
-      Entry(
-        '12월',
-        <Entry>[
-          Entry('9일 이마트24 11:00-16:30'),
-          Entry('10일 이마트24 11:00-16:30'),
-          Entry('25일 이마트24 11:00-16:30'),
-        ],
-      ),
-    ],
-  ),
-];
-
-// Displays one Entry. If the entry has children then it's displayed
-// with an ExpansionTile.
-class EntryItem extends StatelessWidget {
-  const EntryItem(this.entry);
-
-  final Entry entry;
-
-  Widget _buildTiles(Entry root) {
-    if (root.children.isEmpty) return ListTile(title: Text(root.title));
-    return ExpansionTile(
-      key: PageStorageKey<Entry>(root),
-      title: Text(root.title),
-      children: root.children.map(_buildTiles).toList(),
-    );
+  WorkMonthInfo(int month) {
+    this.month = month;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return _buildTiles(entry);
+  WorkMonthInfo.fromJson(Map<String, dynamic> json) {
+    month = json["month"];
+
+    Map toJson() {
+      return {'month': month};
+    }
+  }
+}
+
+class WorkInfo {
+  // 근무일정 클래스
+  String info = ""; // 달을 누르면 나오는 근무기록
+  WorkInfo(String info) {
+    this.info = info;
+  }
+
+  WorkInfo.fromJson(Map<String, dynamic> json) {
+    info = json["info"];
+
+    Map toJson() {
+      return {'info': info};
+    }
   }
 }
