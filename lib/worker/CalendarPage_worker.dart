b0a23286f3e4d6d91b944a413049988e17acb631
hiyoojeong <hiyoojeong@gmail.com>
2022-11-29 21:58:52 +0900

    url 수정

--- lib/worker/CalendarPage_worker.dart
+++ lib/worker/CalendarPage_worker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
@@ -63,6 +64,7 @@ class Calendar extends StatefulWidget {
 class _CalendarState extends State<Calendar> {
   final int MAINCOLOR = 0xffE94869;
   final int SUBCOLOR = 0xffF4F4F4;
+  final int SUBPINKCOLOR = 0xffFDF6F8;
 
   var f = NumberFormat('###,###,###,###');
 
@@ -72,11 +74,18 @@ class _CalendarState extends State<Calendar> {
   String day =
       DateFormat('EEE').format(DateTime.now()).toString().toLowerCase();
 
-  
   var _calendarList = [];
   String token = "", urlsrc = "";
   int userId = 0, storeId = 0;
   String salary = "";
+
+  // 대타요청 하지않은 상태 = 0
+  // 대타요청 한 상태 = 1
+  // 대타수락 된 상태 = 2
+  int daetaState = 0;
+
+  // 대타수락 된 상태면 acceptName에 값이 들어가 있다.
+  String acceptName = "";
 
   _fetchSalary() async {
     // 저장해둔 token 가져오기
@@ -104,7 +113,7 @@ class _CalendarState extends State<Calendar> {
     var response =
         await http.post(Uri.parse(url), headers: headers, body: body);
     var responseBody = utf8.decode(response.bodyBytes);
-    print(responseBody);
+    print("responseBody: " + responseBody);
 
     if (response.statusCode == 200) {
       // 요청 성공
@@ -122,29 +131,126 @@ class _CalendarState extends State<Calendar> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     token = (prefs.getString('token') ?? "null");
     urlsrc = (prefs.getString('urlsrc') ?? "null");
+    userId = (prefs.getInt('userId') ?? 0);
     storeId = (prefs.getInt('storeId') ?? 0);
     print("token: " + token);
     print("urlsrc: " + urlsrc);
+    print("userId: " + userId.toString());
     print("storeId: " + storeId.toString());
 
-    // storeId를 바탕으로 글목록 get 요청
+    // 한달근무표 조회 요청
     String url = "http://${urlsrc}/albba/${storeId}/${day}";
     Map<String, String> headers = {"authorization": "Bearer ${token}"};
     var response = await http.get(Uri.parse(url), headers: headers);
     var responseBody = utf8.decode(response.bodyBytes);
-    print(responseBody);
+    print("responseBody: " + responseBody);
 
     if (response.statusCode == 200) {
       // 요청 성공
       // 전달받은 값 저장
       setState(() {
-        List<dynamic> body = json.decode(responseBody);
-        _calendarList =
-            body.map((dynamic item) => CalendarInfo.fromJson(item)).toList();
+        if (responseBody.toString().isEmpty) {
+          _calendarList = [];
+        } else {
+          List<dynamic> body = json.decode(responseBody);
+          _calendarList =
+              body.map((dynamic item) => CalendarInfo.fromJson(item)).toList();
+        }
       });
     } else {
       // 요청 실패
     }
+  }
+
+  _fetchDeataRequest() async {
+    // 대타 요청
+    String url = "http://${urlsrc}/albba/daeta/request";
+    Map<String, String> headers = {
+      "Content-Type": "application/json",
+      "authorization": "Bearer ${token}"
+    };
+    var body = jsonEncode({
+      "date": DateFormat("yyyyMMdd").format(focusedDay),
+      "storeId": storeId,
+      "requestId": userId
+    });
+    var response =
+        await http.post(Uri.parse(url), headers: headers, body: body);
+    var responseBody = utf8.decode(response.bodyBytes);
+    print(responseBody);
+
+    if (response.statusCode == 200) {
+      // 요청 성공
+    } else {
+      // 요청 실패
+    }
+  }
+
+  _fetchDeataRequestList() async {
+    // 대타 요청리스트 조회
+  }
+
+  _fetchDeataAccept() async {
+    // 대타 수락 리스트 조회
+  }
+
+  showMessage() {
+    showDialog(
+        context: context,
+        barrierDismissible: false, // 창 밖을 터치해도 창이 닫히지 않게
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    child: Text(
+                      "대타요청 하시겠습니까?",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 35,
+                        decoration: BoxDecoration(
+                          color: Color(MAINCOLOR),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextButton(
+                            onPressed: () {
+                              _fetchDeataRequest();
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              "예",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                      ),
+                      Container(
+                        height: 35,
+                        decoration: BoxDecoration(
+                          color: Color(MAINCOLOR),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              "아니오",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   @override
@@ -266,10 +372,8 @@ class _CalendarState extends State<Calendar> {
               Column(
                 children: [
                   for (int i = 0; i < _calendarList.length; i++)
-                    calendarInfo(
-                        (_calendarList[i].name ?? "null"),
-                        (_calendarList[i].start ?? "null"),
-                        (_calendarList[i].end ?? "null"))
+                    calendarInfo(_calendarList[i].userId, _calendarList[i].name,
+                        _calendarList[i].start, _calendarList[i].end)
                 ],
               )
             ],
@@ -279,21 +383,76 @@ class _CalendarState extends State<Calendar> {
     );
   }
 
-  Widget calendarInfo(String name, String start, String end) {
+  Widget calendarInfo(
+      int currentUserId, String name, String start, String end) {
     return Container(
       margin: EdgeInsets.fromLTRB(10, 2, 10, 2),
-      child: Row(
-        children: [
-          Text(
-            start + " - " + end + " ",
-            style: TextStyle(color: Colors.black38, fontSize: 20),
-          ),
-          SizedBox(
-            width: 15,
-          ),
-          Text(name, style: TextStyle(fontSize: 20))
-        ],
-      ),
+      child: Row(children: [
+        Text(
+          start + " - " + end,
+          style: TextStyle(color: Colors.black38, fontSize: 20),
+        ),
+        SizedBox(
+          width: 15,
+        ),
+        Stack(
+          children: [
+            if (daetaState == 2)
+              Text(acceptName, style: TextStyle(fontSize: 20))
+            else
+              Text(name, style: TextStyle(fontSize: 20))
+          ],
+        ),
+        SizedBox(
+          width: 15,
+        ),
+        Stack(
+          children: [
+            if (currentUserId == userId)
+              if (daetaState == 0)
+                GestureDetector(
+                  onTap: () {
+                    showMessage();
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border(
+                            bottom:
+                                BorderSide(color: Color(MAINCOLOR), width: 2))),
+                    child: Text(
+                      "대타요청",
+                      style: TextStyle(
+                          color: Color(MAINCOLOR), fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                )
+              else if (daetaState == 1)
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom:
+                              BorderSide(color: Color(MAINCOLOR), width: 2))),
+                  child: Text(
+                    "대타요청 대기중",
+                    style: TextStyle(
+                        color: Color(MAINCOLOR), fontWeight: FontWeight.w700),
+                  ),
+                )
+              else if (daetaState == 2)
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom:
+                              BorderSide(color: Color(MAINCOLOR), width: 2))),
+                  child: Text(
+                    "대타",
+                    style: TextStyle(
+                        color: Color(MAINCOLOR), fontWeight: FontWeight.w700),
+                  ),
+                )
+          ],
+        ),
+      ]),
     );
   }
 }
